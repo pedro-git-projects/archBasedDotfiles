3107ce92ab90e55625ccce4d538c2f4041da01e4
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-07-14 20:00:39 -0300

    added new workspace tags

--- dwm/config.h
+++ dwm/config.h
@@ -33,10 +33,10 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 /* static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" }; */
 /* static const char *tags[] = { "", "", "", "", "", "", "", "", "" }; */
-/* static const char *tags[] = { "Web", "Chat", "Edit", "Meld", "Vb", "Mail", "Video", "Image", "Files" }; */
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" }; 
 
 
 static const Rule rules[] = {
